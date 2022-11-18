d460fc114fe6c98550f231f3efd73f4227e73d08
Zecil Jain <zecil.jain@gmail.com>
2022-11-18 17:28:10 +0530

    Initial commit

---
+++ emailing.py
@@ -0,0 +1,28 @@
+import smtplib
+import imghdr
+from email.message import EmailMessage
+
+PASSWORD = "tlenjaqzsjuztets"
+SENDER = "zeciljain07@gmail.com"
+RECEIVER = "zeciljain07@gmail.com"
+
+
+def send_email(image_path):
+    print("send_email function started")
+    email_message = EmailMessage()
+    email_message["Subject"] = "New customer showed up!"
+    email_message.set_content("Hey, we just saw a new customer!")
+
+    with open(image_path, "rb") as file:
+        content = file.read()
+
+    email_message.add_attachment(content, maintype="image", subtype=imghdr.what(None, content))
+
+    gmail = smtplib.SMTP("smtp.gmail.com", 587)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(SENDER, PASSWORD)
+    gmail.sendmail(SENDER, RECEIVER, email_message.as_string())
+    gmail.quit()
+    print("send_email function ended")
+
